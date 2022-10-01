8d34c4424962f0e2a0116bc9de93e6a47bb3e069
RUTIK <RUTIK@DESKTOP-Q6KC546>
2022-10-01 13:00:53 +0530

    commit

---
+++ src/sample1/demo1.java
@@ -0,0 +1,17 @@
+package sample1;
+
+public class demo1 {
+
+public static void main(String[] args) {
+	
+System.out.println("hi");
+System.out.println("hi");
+System.out.println("hi");
+System.out.println("hello");
+System.out.println("hello");
+
+
+}
+
+
+}
