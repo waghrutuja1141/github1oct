f86bd11cff64f116c347c6e7e5ce3e6c81d9f62c
RUTIK <RUTIK@DESKTOP-Q6KC546>
2022-10-01 13:50:15 +0530

    [rutuja]:demo3 added class

--- src/sample1/demo2.java
+++ src/sample1/demo2.java
@@ -5,7 +5,7 @@ public class demo2 {
 	public static void main(String[] args) {
 		
 		
-		System.out.print("rutuja shubham");
+		System.out.println("rutuja shubham");
 	}
 	
 	
---
+++ src/sample1/demo3.java
@@ -0,0 +1,11 @@
+package sample1;
+
+public class demo3 {
+
+public static void main(String[] args) {
+	
+	
+	System.out.println("xyz abc");
+}
+
+}
